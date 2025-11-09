a51d324690d0c466bf6c1c8120017890a73abc03
Pusca Vasile <vasea-pusca@mail.ru>
2025-11-09 13:58:21 +0200

    Add exe5

--- src/exe5Parola.java
+++ src/exe5Parola.java
@@ -1,6 +1,14 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class exe5Parola {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input password: ");
+        String parola = input.nextLine();
+        System.out.println("Password was: " + parola);
 
     }
 }
--- src/exe7ParInpar.java
+++ src/exe7ParInpar.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class exe7ParInpar {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int numar =input.nextInt();
+        if (numar % 2 == 0) {
+            System.out.println(numar + " Numarul este par.");
+        }else {
+            System.out.println(numar + " Numarul este inpar.");
+        }
+    }
 }
