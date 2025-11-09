61ee0ca3ebbbbdb8ef420003d88d026ffb7e9ae9
Pusca Vasile <vasea-pusca@mail.ru>
2025-11-09 14:51:38 +0200

    Add exe10

---
+++ src/exe10Majuscule.java
@@ -0,0 +1,9 @@
+public class exe10Majuscule {
+    public static void main(String[] args) {
+
+        String text = "Eu astazi sriu examne";
+        String textMajuscule = text.toUpperCase();
+
+        System.out.println("Text in majuscule: " +textMajuscule);
+    }
+}
